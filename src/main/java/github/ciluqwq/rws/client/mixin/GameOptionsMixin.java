2201dbeaea7ac33c7412da7e154fbb70e35c5efe
CiLuQwQ <a@>
2024-03-24 18:38:08 +0800

    a

--- src/main/java/github/ciluqwq/rws/client/mixin/GameOptionsMixin.java
+++ src/main/java/github/ciluqwq/rws/client/mixin/GameOptionsMixin.java
@@ -1,2 +1,12 @@
-package github.ciluqwq.rws.client.mixin;public class GameOptionsMixin {
+package github.ciluqwq.rws.client.mixin;
+
+import net.minecraft.client.option.GameOptions;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(GameOptions.class)
+public class GameOptionsMixin {
+    @Unique
+    public boolean onboardAccessibility = false;
 }
